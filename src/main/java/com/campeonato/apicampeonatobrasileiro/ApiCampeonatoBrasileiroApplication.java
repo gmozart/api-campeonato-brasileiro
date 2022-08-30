1e6133cae75180eb05ee0f4fc15d2cccd9ae4f54
gmozart <mozart.gleison@gmail.com>
2022-08-30 10:54:55 -0300

    Inicializando API do campeonato Brasileiro

---
+++ src/main/java/com/campeonato/apicampeonatobrasileiro/ApiCampeonatoBrasileiroApplication.java
@@ -0,0 +1,13 @@
+package com.campeonato.apicampeonatobrasileiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCampeonatoBrasileiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCampeonatoBrasileiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campeonato/apicampeonatobrasileiro/ApiCampeonatoBrasileiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campeonato.apicampeonatobrasileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCampeonatoBrasileiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
