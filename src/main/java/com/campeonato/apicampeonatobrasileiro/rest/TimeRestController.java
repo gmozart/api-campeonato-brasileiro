9ac63e8bdfeb0009972e15eca886569dcab21843
gmozart <mozart.gleison@gmail.com>
2022-09-02 17:29:57 -0300

    Adicionando notação de POST

--- src/main/java/com/campeonato/apicampeonatobrasileiro/rest/TimeRestController.java
+++ src/main/java/com/campeonato/apicampeonatobrasileiro/rest/TimeRestController.java
@@ -2,12 +2,11 @@ package com.campeonato.apicampeonatobrasileiro.rest;
 
 import com.campeonato.apicampeonatobrasileiro.entity.Time;
 import com.campeonato.apicampeonatobrasileiro.service.TimeService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,16 +17,19 @@ public class TimeRestController {
     @Autowired
     private TimeService timeServico;
 
+    @ApiOperation(value = "Obtém os dados de um time")
     @GetMapping(value = "{id}")
     public ResponseEntity<Time> getTime(@PathVariable Integer id){
       return ResponseEntity.ok().body(timeServico.obterTime(id));
     }
 
+    @ApiOperation(value = "Obtém uma lista de Times")
     @GetMapping(value = "{}")
     public ResponseEntity<List <Time>>  getTimes(){
       return ResponseEntity.ok().body(timeServico.listarTimes());
     }
 
+    @PostMapping
     public ResponseEntity<Void> cadastrarTime(Time time){
         timeServico.cadastrarTime(time);
       return ResponseEntity.ok().build();
