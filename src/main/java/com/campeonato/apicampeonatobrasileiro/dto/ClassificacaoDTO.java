08222f9f1ec05e524bb97c7106633074e169de33
gmozart <mozart.gleison@gmail.com>
2022-09-06 01:32:39 -0300

    Atribuindo os atributos da classe classificacaoDTO

--- src/main/java/com/campeonato/apicampeonatobrasileiro/dto/ClassificacaoDTO.java
+++ src/main/java/com/campeonato/apicampeonatobrasileiro/dto/ClassificacaoDTO.java
@@ -5,6 +5,16 @@ import lombok.Data;
 @Data
 public class ClassificacaoDTO {
 
+    private String time;
+    private Integer idTime;
+    private Integer posicao;
+    private Integer pontos;
+    private Integer jogos;
+    private Integer vitorias;
+    private Integer empates;
+    private Integer derrotas;
+    private Integer golsMarcados;
+    private Integer golsSofridos;
 
 
 }
